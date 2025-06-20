d14e84448016e88684ab5da454ae30f79f3f0d24
AlexeiGolubev <fir258@yandex.ru>
2025-06-20 19:20:04 +0400

    KS-1-2 Simulating logs from the server

---
+++ src/main/kotlin/lesson_01/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_01
+
+fun main() {
+    val numberOfOrders: Int = 75
+
+    val notificationTextForCustomer: String = "Благодарим Вас за покупку!"
+
+    println("Количество заказов в интернет-магазине: $numberOfOrders")
+    println("Текст с благодарностью за покупку: $notificationTextForCustomer")
+
+    var numberOfEmployees: Int = 2000
+
+    //println("Количество работников данного интернет-магазина: $numberOfEmployees")
+
+    numberOfEmployees--
+
+    println("Количество работников данного интернет-магазина: $numberOfEmployees")
+
+}
