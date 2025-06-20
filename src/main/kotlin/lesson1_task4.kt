1b1cfa4fe3b9910c0fe3100f9d2c60042aaee35e
AlexeiGolubev <fir258@yandex.ru>
2025-06-20 20:44:33 +0400

    KS-1-4 create different types of variable

---
+++ src/main/kotlin/lesson1_task4.kt
@@ -0,0 +1,18 @@
+package org.example
+
+fun main() {
+    val distanceOfGagarinSpaceFlightInMillimeters: Long = 40_868_600_000
+    val ageOfGagarinAtTimeOfSpaceFlight: Byte = 27
+    val partOfDayOfGagarinSpaceFlight: Float = 0.075f
+    val numberOfSecondsOfGagarinSpaceFlight: Short = 6480
+    val partOfYearOfGagarinSpaceFlight: Double = 2.0547945205479453E-4
+    val apogeeOfGagarinOrbitInMeters: Int = 327000
+
+    println(distanceOfGagarinSpaceFlightInMillimeters)
+    println(ageOfGagarinAtTimeOfSpaceFlight)
+    println(partOfDayOfGagarinSpaceFlight)
+    println(numberOfSecondsOfGagarinSpaceFlight)
+    println(partOfYearOfGagarinSpaceFlight)
+    println(apogeeOfGagarinOrbitInMeters)
+
+}
