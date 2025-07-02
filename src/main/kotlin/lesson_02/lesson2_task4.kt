534a67d67eb99fd30f2b364ab8c82ade6c1d586e
AlexeiGolubev <fir258@yandex.ru>
2025-07-02 16:50:34 +0400

    KS-2-4 adding constant

--- src/main/kotlin/lesson_02/lesson2_task4.kt
+++ src/main/kotlin/lesson_02/lesson2_task4.kt
@@ -1,5 +1,7 @@
 package org.example.lesson_02
 
+const val PERCENTAGE_VALUE = 100
+
 //В мобильной стратегии есть бафф (временное усиление какого-либо юнита или процесса), который дает +20% к добытым материалам. Было добыто 7 кристаллической и 11 железной руды без баффа – заранее известные данные, объявить и проинициализировать.
 //
 //Напиши программу, которая будет учитывать действие баффа и выводить в консоль количество “бонусных” (то есть тех материалов, которые даются “сверху” баффом). По одной строке на материал с его названием.
@@ -10,8 +12,8 @@ fun main() {
     val numberOfCrystalOre = 7
     val numberOfIronOre = 11
     val buffSize = 20
-    val numberOfCrystalOreWithBuff = numberOfCrystalOre * 20 / 100
-    val numberOfIronOreWithBuff = numberOfIronOre * 20 / 100
+    val numberOfCrystalOreWithBuff = numberOfCrystalOre * buffSize / PERCENTAGE_VALUE
+    val numberOfIronOreWithBuff = numberOfIronOre * buffSize / PERCENTAGE_VALUE
 
     println("Было добыто $numberOfCrystalOre кристаллической и $numberOfIronOre железной руды без баффа.")
     println("Бафф (временное усиление какого-либо юнита или процесса), который дает +$buffSize% к добытым материалам.")
